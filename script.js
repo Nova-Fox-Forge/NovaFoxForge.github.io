4a9532ef399d51acc839a42019350acc7380bbb1
FoxBandyKoot <lerenard.charly@gmail.com>
2024-03-17 15:35:43 +0100

    fix the disappearance of blocks and add description of the third block

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ alert("Site web en construction");
 /* Buttons */
 function upOpacity(id) {
     var columnToDisplay = document.getElementById(id);
-    var otherColumns = document.querySelectorAll('.main_column:not(#' + id + ')');
+    var otherColumns = document.querySelectorAll('.menu:not(#' + id + ')');
 
     // Augmente l'opacité de la div avec l'id 'X' de 0 à 100%
     columnToDisplay.style.opacity = '1';
@@ -11,6 +11,5 @@ function upOpacity(id) {
     // Réduit l'opacité des autres divs de 100 à 0% puis les cache
     otherColumns.forEach(function(div) {
         div.style.opacity = '0';
-        div.classList.add('collapsed');
     });
 }
